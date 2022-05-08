25335a1290f0f5809df5b5f6da9a24768afd3184
neutral-flex <neutralflex@gmail.com>
2022-05-08 13:22:32 +0500

    Initial commit

---
+++ Assets/Scripts/ColorChooser.cs
@@ -0,0 +1,46 @@
+﻿using Es.InkPainter;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ColorChooser : MonoBehaviour
+{
+    public Animator f;
+    public static ColorChooser i;
+    public static Color col;
+    public Color Col;
+    public Slider sl;
+    public Slider sliderForWater;
+    public Texture a;
+
+    public Texture[] shapes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Col = col;
+    }
+    public void BrushColor(Image im)
+    {
+        col = im.color;
+    }
+    public void ShapeChanger(int num)
+    {
+        a = shapes[num];
+    }
+    public void NextFlip()
+    {
+        f.SetTrigger("On");
+    }
+    public void Resett()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    public GameObject postCardBack;
+    public GameObject Hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ItsFlipTime()
+    {
+        postCardBack.SetActive(true);
+        gameObject.SetActive(false);
+
+    }
+    public void Hide()
+    {
+        Hand.SetActive(false);
+    }
+}
